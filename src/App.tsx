78808b1ed364a0e623bcb0d9ae0c5bc474820c32
Michaela <mi-and@outlook.com>
2024-08-28 19:03:13 +0200

    PhotoBox component and button styling

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,20 @@ import Card from "./components/Card";
 import Header from "./components/Header";
 import InfoBox from "./components/InfoBox";
 import NewLetterBox from "./components/NewsLetterBox";
+import PhotoBox from "./components/PhotoBox";
 
 function App() {
   return (
     <div className="bg-[#FAF9F9]">
       <Header />
-      <div className="w-full h-72 sm:h-80 md:h-96 lg:h-[28rem] overflow-hidden">
+      <PhotoBox />
+      {/* <div className="w-full h-72 sm:h-80 md:h-96 lg:h-[28rem] overflow-hidden">
         <img
           src="https://plus.unsplash.com/premium_photo-1661277693458-6c65cb09aa41?q=80&w=1888&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
           alt="bild"
           className="w-full h-full object-cover"
         />
-      </div>
+      </div> */}
       <InfoBox />
       <Card />
       <NewLetterBox />
---
+++ src/components/PhotoBox.tsx
@@ -0,0 +1,21 @@
+function PhotoBox() {
+  return (
+    <div
+      className="w-full h-72 sm:h-80 md:h-96 lg:h-[28rem] bg-cover bg-center flex justify-center items-center flex-col"
+      style={{
+        backgroundImage: `url('https://plus.unsplash.com/premium_photo-1661277693458-6c65cb09aa41?q=80&w=1888&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')`,
+      }}
+    >
+      <div className="bg-white bg-opacity-40 w-2/3 xl:w-1/2 flex flex-col justify-center items-center p-6">
+        <h1 className="text-black text-3xl mb-4 uppercase">
+          Njut i en stilla oas
+        </h1>
+        <button className="bg-[#FAA916] text-white py-4 px-4 w-1/3 m-2 uppercase drop-shadow-xl hover:opacity-80 transform hover:scale-110 transition-transform duration-100">
+          Boka
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default PhotoBox;
